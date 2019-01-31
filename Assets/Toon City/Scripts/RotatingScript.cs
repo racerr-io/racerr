c61e3c85f3619fd753fbeb2a26ab571de84d48c1
Tony Tang <tonytang215@hotmail.com>
2019-01-31 14:55:24 +1100

    Added Toon City Assets

---
+++ Assets/Toon City/Scripts/RotatingScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingScript : MonoBehaviour
+{
+    public float xRotation;
+    public float yRotation;
+    public float zRotation;
+
+    public float SpeedRotation = 5f;
+
+    private Vector3 _currentRotation;
+
+    void Start()
+    {
+        _currentRotation = gameObject.transform.rotation.eulerAngles;
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(xRotation, yRotation, zRotation), SpeedRotation);
+    }
+}
