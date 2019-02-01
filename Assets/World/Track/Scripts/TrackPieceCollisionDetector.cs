385219354507a1b85b1aa29fee2334f90cd57ccc
RFSurdsman <ryan_fallah@hotmail.com>
2019-02-01 12:44:58 +1100

    Small chang

--- Assets/World/Track/Scripts/TrackPieceCollisionDetector.cs
+++ Assets/World/Track/Scripts/TrackPieceCollisionDetector.cs
@@ -6,14 +6,16 @@ namespace Racerr.Track
 {
     public class TrackPieceCollisionDetector : MonoBehaviour
     {
-        public bool IsValidTrackPlacementUponConnection => CollidedGameObjects.Where(g => g != null).Count() <= 1; // Is valid placement given we haven't connected it to the next piece.
-        List<GameObject> CollidedGameObjects { get; set; } = new List<GameObject>();
+        public bool IsValidTrackPlacementUponConnection => CollidedGameObjects.Where(g => g != null).Count() <= 1;
+        List<GameObject> CollidedGameObjects { get; } = new List<GameObject>();
 
         void OnCollisionEnter(Collision collision)
         {
-            if (collision.gameObject.CompareTag(TrackPieceComponent.TrackPiece))
+            GameObject collidedGameObject = collision.gameObject;
+
+            if (collidedGameObject.CompareTag(TrackPieceComponent.TrackPiece) && !CollidedGameObjects.Contains(collidedGameObject))
             {
-                CollidedGameObjects.Add(collision.gameObject);
+                CollidedGameObjects.Add(collidedGameObject);
             }
         }
     }
