ef0fd9fe722888b7ceafadca7e6a857e7c251830
RFSurdsman <ryan_fallah@hotmail.com>
2019-02-06 13:30:43 +1100

    Racerr Network Transform Interpolation

---
+++ Assets/Cars/Race Car/RacerrNetworkTransform.cs
@@ -0,0 +1,51 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class RacerrNetworkTransform : NetworkBehaviour
+{
+    [SyncVar] Vector3 RealPosition = Vector3.zero;
+    [SyncVar] Quaternion RealRotation;
+    [SyncVar] Vector3 RealVelocity;
+
+    [SerializeField] [Range(0,1)] float InterpolationFactor = 0.4f;
+    Rigidbody Rigidbody { get; set; }
+
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+
+        if (!isLocalPlayer)
+        {
+            foreach (WheelCollider wheelCollider in GetComponentsInChildren<WheelCollider>())
+            {
+                Destroy(wheelCollider);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            RealPosition = transform.position;
+            RealRotation = transform.rotation;
+            RealVelocity = Rigidbody.velocity;
+            CmdSync(transform.position, transform.rotation, Rigidbody.velocity);
+        }
+        else
+        {
+            Vector3 predictedPosition = RealPosition + Time.deltaTime * RealVelocity;
+            transform.position = Vector3.Lerp(transform.position, predictedPosition, InterpolationFactor);
+            transform.rotation = Quaternion.Lerp(transform.rotation, RealRotation, InterpolationFactor);
+            Rigidbody.velocity = RealVelocity;
+        }
+    }
+
+    [Command]
+    void CmdSync(Vector3 position, Quaternion rotation, Vector3 velocity)
+    {
+        RealPosition = position;
+        RealRotation = rotation;
+        RealVelocity = velocity;
+    }
+}
