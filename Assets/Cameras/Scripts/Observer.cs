7fe581d8f6ec42fbdbe5e526278ced33b9118b48
Jimmy Chen <50786287+Q16solver@users.noreply.github.com>
2019-05-19 17:14:29 +1000

    #60 Created Observer Script
    Created a ray that starts from the camera and has direction going to player car

---
+++ Assets/Cameras/Scripts/Observer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 direction = Player.LocalPlayer.Car.position - transform.position + Vector3.up;
+        Ray ray = new Ray(transform.position, direction);
+        RaycastHit raycastHit;
+    }
+}
