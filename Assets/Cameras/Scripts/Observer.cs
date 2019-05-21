3eb01ffd2a6a45ddd8a0f8712b536d568a075117
RFSurdsman <ryan_fallah@hotmail.com>
2019-05-21 13:11:01 +1000

    #60 Building state transparent

--- Assets/Cameras/Scripts/Observer.cs
+++ Assets/Cameras/Scripts/Observer.cs
@@ -1,13 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Racerr.Car.Core;
+using Racerr.MultiplayerService;
 using UnityEngine;
 
 public class Observer : MonoBehaviour
 {
     void Update()
     {
-        Vector3 direction = Player.LocalPlayer.Car.position - transform.position + Vector3.up;
-        Ray ray = new Ray(transform.position, direction);
-        RaycastHit raycastHit;
-    }
+        PlayerCarController car = Player.LocalPlayer?.Car;
+
+        if (car != null)
+        {
+            Vector3 direction = Player.LocalPlayer.Car.transform.position - Camera.main.transform.position;
+            RaycastHit raycastHit;
+            if (Physics.Raycast(Camera.main.transform.position, direction, out raycastHit))
+            {
+                BuildingState state = raycastHit.collider.gameObject.GetComponent<BuildingState>();
+                if (state != null)
+                {
+                    state.HitRay();
+                }
+            }
+        }
+    }        
 }
+
