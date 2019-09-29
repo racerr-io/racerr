6145fe552fe1517c7b72dc3ee25224fafa0d5194
Ryan Fallah <ryan_fallah@hotmail.com>
2019-09-29 15:26:18 +1000

    #82 Ensure You Can't Steer When You Are Dead

--- Assets/Cars/Scripts/PlayerCarController.cs
+++ Assets/Cars/Scripts/PlayerCarController.cs
@@ -86,9 +86,13 @@ namespace Racerr.Car.Core
                 GetInput();
             }
 
-            Steer();
-            Drive();
-            UpdateWheelPositions();
+            if (!Player.IsDead)
+            {
+                Steer();
+                Drive();
+                UpdateWheelPositions();
+            }
+            
             AddDownForce();
             UpdateSidewaysFrictionWithSpeed();
         }
@@ -122,8 +126,15 @@ namespace Racerr.Car.Core
         /// </summary>
         void GetInput()
         {
-            HorizontalInput = Input.GetAxis("Horizontal");
-            VerticalInput = Input.GetAxis("Vertical");
+            if (!Player.IsDead)
+            {
+                HorizontalInput = Input.GetAxis("Horizontal");
+                VerticalInput = Input.GetAxis("Vertical");
+            }
+            else
+            {
+                HorizontalInput = VerticalInput = 0;
+            }
         }
 
         /// <summary>
@@ -170,11 +181,6 @@ namespace Racerr.Car.Core
         /// </summary>
         void Drive()
         {
-            if (Player.IsDead)
-            {
-                return;
-            }
-
             Vector3 localVel = transform.InverseTransformDirection(rigidbody.velocity);
 
             if (VerticalInput >= 0 || (localVel.z < 0 && VerticalInput <= 0))
