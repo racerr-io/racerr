0a2a5507752b406f92999d1a233802636df5f869
Jimmy Chen <50786287+Q16solver@users.noreply.github.com>
2019-12-30 20:08:25 +1100

    #113 refactored code into function and updated summary

--- Assets/Infrastructure/Client/ClientSpectateState.cs
+++ Assets/Infrastructure/Client/ClientSpectateState.cs
@@ -11,27 +11,21 @@ namespace Racerr.Infrastructure.Client
     public class ClientSpectateState : LocalState
     {
         Player playerBeingSpectated;
-        IEnumerable<Player> playersInRace;
+        IEnumerable<Player> playersInRace = null;
 
         /// <summary>
-        /// Upon entering the spectate state on the client, find the players in the race that we can spectate.
-        /// We can assume this to be non-empty as the race would already be finished otherwise and we would transition to intermission state.
-        /// From the players in the race, we choose the first player to spectate.
+        /// Upon entering the spectate state on the client, set the first player in the race to be spectated.
         /// </summary>
         /// <param name="optionalData">Should be null</param>
         public override void Enter(object optionalData = null)
         {
-            playersInRace = FindObjectsOfType<Player>().Where(player => !player.IsDead && !player.PosInfo.IsFinished);
-            playerBeingSpectated = playersInRace.First();
-            ClientStateMachine.Singleton.SetPlayerCameraTarget(playersInRace.First().CarManager.transform);
-            ClientStateMachine.Singleton.SetMinimapCameraTarget(playersInRace.First().CarManager.transform);
+            SetPlayerBeingSpectated();
         }
 
         /// <summary>
         /// Called every physics tick to monitor the server state. If the server has changed to intermission,
         /// it means we can join the next race! Hence, update our UI to the Intermission State.
-        /// If the player we are spectating leaves the game or dies, we recheck the players in the race to find
-        /// the first player to spectate.
+        /// If the player we are spectating leaves the game or dies, we refind the new first player to spectate.
         /// </summary>
         protected override void FixedUpdate()
         {
@@ -41,10 +35,7 @@ namespace Racerr.Infrastructure.Client
             }
             else if (playerBeingSpectated == null || playerBeingSpectated.IsDead)
             {
-                playersInRace = playersInRace.Where(player => player != null && !player.IsDead && !player.PosInfo.IsFinished);
-                playerBeingSpectated = playersInRace.First();
-                ClientStateMachine.Singleton.SetPlayerCameraTarget(playersInRace.First().CarManager.transform);
-                ClientStateMachine.Singleton.SetMinimapCameraTarget(playersInRace.First().CarManager.transform);
+                SetPlayerBeingSpectated();
             }
 
         }
@@ -52,6 +43,27 @@ namespace Racerr.Infrastructure.Client
         void TransitionToIntermission()
         {
             ClientStateMachine.Singleton.ChangeState(StateEnum.Intermission);
+        }
+
+        /// <summary>
+        /// Find the players in the race that we can spectate.
+        /// We can assume this to be non-empty as the race would already be finished otherwise and we would transition to intermission state.
+        /// From the players in the race, we choose the first player to spectate.
+        /// </summary>
+        void SetPlayerBeingSpectated()
+        {
+            if (playersInRace == null)
+            {
+                playersInRace = FindObjectsOfType<Player>().Where(player => player != null && !player.IsDead && !player.PosInfo.IsFinished);
+            }
+            else
+            {
+                playersInRace = playersInRace.Where(player => player != null && !player.IsDead && !player.PosInfo.IsFinished);
+            }
+
+            playerBeingSpectated = playersInRace.First();
+            ClientStateMachine.Singleton.SetPlayerCameraTarget(playersInRace.First().CarManager.transform);
+            ClientStateMachine.Singleton.SetMinimapCameraTarget(playersInRace.First().CarManager.transform);
         }
     }
 }
