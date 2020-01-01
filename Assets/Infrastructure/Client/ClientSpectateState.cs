dae88a2c80583edfa0e2a7146b14ec133f22175b
Jimmy Chen <50786287+Q16solver@users.noreply.github.com>
2020-01-02 10:58:34 +1100

    #113 added manual spectating camera change through spacebar

--- Assets/Infrastructure/Client/ClientSpectateState.cs
+++ Assets/Infrastructure/Client/ClientSpectateState.cs
@@ -23,6 +23,7 @@ namespace Racerr.Infrastructure.Client
         [SerializeField] SpectatedPlayerNameUIComponent spectatedPlayerNameUIComponent;
 
         IEnumerable<Player> playersInRace = null;
+        IEnumerable<Player> unspectatedPlayersInRace = null;
         Player spectatedPlayer;
 
         /// <summary>
@@ -34,6 +35,7 @@ namespace Racerr.Infrastructure.Client
             raceView.Show();
             spectatedPlayerNameUIComponent.gameObject.SetActive(true);
             playersInRace = FindObjectsOfType<Player>().Where(player => player != null && !player.IsDead && !player.PosInfo.IsFinished);
+            unspectatedPlayersInRace = playersInRace;
         }
 
         /// <summary>
@@ -41,31 +43,54 @@ namespace Racerr.Infrastructure.Client
         /// </summary>
         protected override void FixedUpdate()
         {
-            SetSpectateTargetIfRequired();
+            SetSpectatedPlayerIfRequired();
             UpdateUIComponents();
             CheckToTransition();
         }
 
         /// <summary>
-        /// Find a player in the race that we can spectate. From the players in the race, we choose the first player to spectate, ensuring
-        /// they haven't finished, died or left the server. The current spectated player's name will be displayed.
+        /// If we are not spectating or our current spectated player is dead, we choose a player in the race
+        /// to spectate, ensuring that they haven't finished, died or left the server. If the spectating player 
+        /// decides to change the spectated player (through pressing spacebar), we further ensure spectated
+        /// players are not repeated until all players still in the race have been spectated.
         /// </summary>
-        void SetSpectateTargetIfRequired()
+        void SetSpectatedPlayerIfRequired()
         {
             if (spectatedPlayer == null || spectatedPlayer.IsDead)
             {
-                // playersInRace could be empty, as there is a small window of time where everyone has died/finished but the Server State Machine
-                // has not transitioned to intermission yet.
                 playersInRace = playersInRace.Where(player => player != null && !player.IsDead && !player.PosInfo.IsFinished);
-                spectatedPlayer = playersInRace.FirstOrDefault(); 
-                ClientStateMachine.Singleton.SetPlayerCameraTarget(spectatedPlayer?.CarManager.transform);
-                minimapUIComponent.SetMinimapCameraTarget(ClientStateMachine.Singleton.LocalPlayer.CarManager.transform);
+                SetSpectatedPlayer(playersInRace);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space)) 
+            {
+                unspectatedPlayersInRace = unspectatedPlayersInRace.Where(player => player != spectatedPlayer && player != null && !player.IsDead && !player.PosInfo.IsFinished);
+                if (unspectatedPlayersInRace == null)
+                {
+                    unspectatedPlayersInRace = playersInRace;
+                }
+                SetSpectatedPlayer(unspectatedPlayersInRace);
             }
         }
 
         /// <summary>
-        /// Update all the UI components in the client race view, which shows information about the spectated player's car and how they 
-        /// are performing in the race.
+        /// Find the first player that we can spectate.
+        /// </summary>
+        /// <remarks>
+        /// spectatablePlayers could be empty, as there is a small window of time where everyone has died/finished 
+        /// but the Server State Machine has not transitioned to intermission yet.
+        /// </remarks>
+        /// <param name="spectatablePlayers"></param>
+        void SetSpectatedPlayer(IEnumerable<Player> spectatablePlayers)
+        {
+            spectatedPlayer = spectatablePlayers.FirstOrDefault();
+            ClientStateMachine.Singleton.SetPlayerCameraTarget(spectatedPlayer?.CarManager.transform);
+            minimapUIComponent.SetMinimapCameraTarget(ClientStateMachine.Singleton.LocalPlayer.CarManager.transform);
+        }
+
+        /// <summary>
+        /// Update all the UI components in the client race view, which shows information about the spectated player's car 
+        /// and how they are performing in the race.
         /// </summary>
         void UpdateUIComponents()
         {
