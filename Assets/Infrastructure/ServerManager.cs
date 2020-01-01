2c0cb1f85aca5f683752644a70efac7e34100597
Jimmy Chen <50786287+Q16solver@users.noreply.github.com>
2020-01-01 21:27:39 +1100

    Refactor/#122/debug multiplayer functionality (#123)
    * #122 Added debug modes and corresponding if conditions
    
    * #122 Refactored debug mode code and conditions
    
    * #122 Removed unnecessary/wrong code + refactoring
    
    * #122 refactored code into one liners
    
    * #122 trivial line
    
    * #122 refactored debug modes into function and updated summary
    
    * (#122) Restructure Server Manager
    
    * (#122) Comments
    
    * #122 Moved unity editor specific functions together
    
    * #122 Refactored code by replacing #if unity editor lines
    
    * (#112) Fixed case where debugging using 2 editors doesn't work
    
    * (#122) Accidentally mispositioned the countdown timer in earlier commit, now fixed.
    
    Co-authored-by: Ryan Fallah <30363274+RFSurdsman@users.noreply.github.com>

--- Assets/Infrastructure/ServerManager.cs
+++ Assets/Infrastructure/ServerManager.cs
@@ -1,6 +1,7 @@
 ﻿using Mirror;
 using UnityEngine;
 using Racerr.Infrastructure.Server;
+using System;
 
 namespace Racerr.Infrastructure
 {
@@ -10,39 +11,90 @@ namespace Racerr.Infrastructure
     public class ServerManager : NetworkManager
     {
         [Header("Other")]
-        [SerializeField] GameObject[] destroyOnServerLoad;
+        [SerializeField] GameObject[] destroyOnHeadlessLoad;
+        [SerializeField] EditorDebugModeEnum editorDebugMode;
+
+        const string localServerAddress = "localhost";
+        enum EditorDebugModeEnum
+        {
+            Host,
+            Headless,
+            ClientLocal,
+            ClientOnline
+        }
 
         /// <summary>
-        /// Automatically start the server on headless mode (detected through whether it has a graphics device),
-        /// or automatically connect client on server on normal mode.
+        /// On game start, initialise the networking infrastructure. If you are running in the Unity Editor, we will
+        /// use debug mode, which allows you to test the game in various environments without needing to deploy to AWS.
         /// </summary>
         public override void Start()
         {
-            if (isHeadless)
+            if (Application.isEditor)
             {
-                Application.targetFrameRate = serverTickRate;
-                DestroySelectedGameObjectsOnServerLoad();
-                StartServer();
+                InitialiseDebugModeNetworking();
             }
             else
             {
-#if UNITY_EDITOR
-                StartHost();
-#else
-                StartClient();
-#endif
+                InitialiseNetworking();
             }
         }
 
         /// <summary>
-        /// Remove game objects which are only specific to the client, to optimise server performance.
+        /// Initialises networking in debug mode, intended for use when working with the game in the Unity Editor. You may
+        /// select the desired debugging mode via the dropdown in the inspector.
         /// </summary>
-        void DestroySelectedGameObjectsOnServerLoad()
+        /// <remarks>
+        /// Possible debugging modes:
+        /// Host - Start as both the server and the client.
+        /// Headless - Headless server mode (<see cref="StartHeadless"/>).
+        /// Client Local - Client connects to localhost instead of the specified networkAddress, so that we can connect to 
+        /// another editor which is running in Host or Headless mode.
+        /// Client Online - Client connects to the online server and acts as a client, so that we can play on racerr.io 
+        /// through the Unity Editor.
+        /// </remarks>
+        void InitialiseDebugModeNetworking()
         {
-            foreach (GameObject gameObject in destroyOnServerLoad)
+            switch (editorDebugMode)
+            {
+                case EditorDebugModeEnum.Host: StartHost(); break;
+                case EditorDebugModeEnum.Headless: StartHeadless(); break;
+                case EditorDebugModeEnum.ClientOnline: StartClient(); break;
+                case EditorDebugModeEnum.ClientLocal: networkAddress = localServerAddress; StartClient(); break;
+                default: throw new InvalidOperationException("Invalid Unity Editor Debug Mode attempt: " + editorDebugMode);
+            }
+        }
+
+        /// <summary>
+        /// Initialises networking for the compiled version of the game. If we have no graphics card (i.e. having no head), 
+        /// we will assume we are a headless server (<see cref="StartHeadless"/>), otherwise we are a client.
+        /// </summary>
+        void InitialiseNetworking()
+        {
+            if (isHeadless)
+            {
+                StartHeadless();
+            }
+            else
+            {
+                StartClient();
+            }
+        }
+
+        /// <summary>
+        /// Special mode designed especially for deployment to a server. This will destroy all GameObjects
+        /// which are only useful for the client (such as the UI) to optimise server performance.
+        /// We also limit the frame rate, as there is no point having the frame rate of the server be higher
+        /// than the server update frequency.
+        /// </summary>
+        void StartHeadless()
+        {
+            foreach (GameObject gameObject in destroyOnHeadlessLoad)
             {
                 Destroy(gameObject);
             }
+
+            Application.targetFrameRate = serverTickRate;
+            StartServer();
         }
 
         /// <summary>
