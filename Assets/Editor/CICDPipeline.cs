768c788ae72290fc6b56de0304893ea9e48d0fa8
Ryan Fallah <ryan_fallah@hotmail.com>
2020-04-23 20:34:42 +1000

    Better pipeline

--- Assets/Editor/CICDPipeline.cs
+++ Assets/Editor/CICDPipeline.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
 
-namespace UnityBuilderAction
+namespace Racerr.Editor
 {
-    static class Builder
+    /// <summary>
+    /// Used by the CI/CD pipeline to report the build status to the console and build the game.
+    /// Pipeline will execute Unity from the command line and call BuildProject.
+    /// Unity command line docs: https://docs.unity3d.com/Manual/CommandLineArguments.html.
+    /// </summary>
+    static class CICDPipeline
     {
-        private static string EOL = Environment.NewLine;
+        static readonly string EOL = Environment.NewLine;
 
-        private static void ParseCommandLineArguments(out Dictionary<string, string> providedArguments)
+        /// <summary>
+        /// Custom parameters to configure the build. Parameters must start with a hyphen (-) and may be followed by a value (without hyphen). 
+        /// Parameters without a value will be considered booleans (with a value of true).
+        /// e.g. -profile SomeProfile -someBoolean -someValue exampleValue
+        /// </summary>
+        /// <param name="providedArguments">Key value pairs of arguments.</param>
+        static void ParseCommandLineArguments(out Dictionary<string, string> providedArguments)
         {
             providedArguments = new Dictionary<string, string>();
             string[] args = Environment.GetCommandLineArgs();
@@ -41,17 +52,22 @@ namespace UnityBuilderAction
             }
         }
 
-        private static Dictionary<string, string> GetValidatedOptions()
+        /// <summary>
+        /// Returns validated command line arguments. Validation ensures essential arguments
+        /// are passed so the build does not fail.
+        /// </summary>
+        /// <returns>Updated command line arguments.</returns>
+        static Dictionary<string, string> GetValidatedOptions()
         {
-            ParseCommandLineArguments(out var validatedOptions);
+            ParseCommandLineArguments(out Dictionary<string, string> validatedOptions);
 
-            if (!validatedOptions.TryGetValue("projectPath", out var projectPath))
+            if (!validatedOptions.TryGetValue("projectPath", out _))
             {
                 Console.WriteLine("Missing argument -projectPath");
                 EditorApplication.Exit(110);
             }
 
-            if (!validatedOptions.TryGetValue("buildTarget", out var buildTarget))
+            if (!validatedOptions.TryGetValue("buildTarget", out string buildTarget))
             {
                 Console.WriteLine("Missing argument -buildTarget");
                 EditorApplication.Exit(120);
@@ -62,48 +78,42 @@ namespace UnityBuilderAction
                 EditorApplication.Exit(121);
             }
 
-            if (!validatedOptions.TryGetValue("buildOptions", out var buildOptions))
+            if (!validatedOptions.TryGetValue("buildOptions", out string buildOptions) || !Enum.IsDefined(typeof(BuildOptions), buildOptions))
             {
-                Console.WriteLine("Missing argument -buildOptions");
-                EditorApplication.Exit(120);
+                const string defaultBuildOptions = "None";
+                Console.WriteLine($"Missing or invalid argument -buildOptions, defaulting to {defaultBuildOptions}.");
+                validatedOptions.Add("buildOptions", defaultBuildOptions);
             }
 
-            if (!Enum.IsDefined(typeof(BuildOptions), buildOptions))
-            {
-                EditorApplication.Exit(121);
-            }
-
-            if (!validatedOptions.TryGetValue("customBuildPath", out var customBuildPath))
+            if (!validatedOptions.TryGetValue("customBuildPath", out _))
             {
                 Console.WriteLine("Missing argument -customBuildPath");
                 EditorApplication.Exit(130);
             }
 
-            string defaultCustomBuildName = "TestBuild";
-            if (!validatedOptions.TryGetValue("customBuildName", out var customBuildName))
+            if (!validatedOptions.TryGetValue("customBuildName", out string customBuildName) || customBuildName == "")
             {
+                const string defaultCustomBuildName = "TestBuild";
                 Console.WriteLine($"Missing argument -customBuildName, defaulting to {defaultCustomBuildName}.");
-                validatedOptions.Add("customBuildName", defaultCustomBuildName);
-            }
-            else if (customBuildName == "")
-            {
-                Console.WriteLine($"Invalid argument -customBuildName, defaulting to {defaultCustomBuildName}.");
                 validatedOptions.Add("customBuildName", defaultCustomBuildName);
             }
 
             return validatedOptions;
         }
 
-        public static void BuildProject()
+        /// <summary>
+        /// Builds the project. Can only be executed by pipeline.
+        /// </summary>
+        static void BuildProject()
         {
             // Gather values from args
-            var options = GetValidatedOptions();
+            Dictionary<string, string> options = GetValidatedOptions();
 
             // Gather values from project
-            var scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(s => s.path).ToArray();
+            string[] scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(s => s.path).ToArray();
 
             // Define BuildPlayer Options
-            var buildOptions = new BuildPlayerOptions
+            BuildPlayerOptions buildOptions = new BuildPlayerOptions
             {
                 scenes = scenes,
                 locationPathName = options["customBuildPath"],
@@ -123,7 +133,11 @@ namespace UnityBuilderAction
             ExitWithResult(result);
         }
 
-        private static void ReportSummary(BuildSummary summary)
+        /// <summary>
+        /// Prints a summary of the build after it is done.
+        /// </summary>
+        /// <param name="summary">Build summary.</param>
+        static void ReportSummary(BuildSummary summary)
         {
             Console.WriteLine(
               $"{EOL}" +
@@ -139,7 +153,11 @@ namespace UnityBuilderAction
             );
         }
 
-        private static void ExitWithResult(BuildResult result)
+        /// <summary>
+        /// Prints the build result and exits with the correct code.
+        /// </summary>
+        /// <param name="result">Build result.</param>
+        static void ExitWithResult(BuildResult result)
         {
             if (result == BuildResult.Succeeded)
             {
