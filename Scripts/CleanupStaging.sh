4023974cb87305783e349b70731cc9fe266e3168
Ryan Fallah <30363274+RFSurdsman@users.noreply.github.com>
2020-04-30 00:41:33 +1000

    Refactor Staging Strategy

---
+++ Scripts/CleanupStaging.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# run this script from the root of the repository using npm run getBuild
+
+AUTH_HEADER=""
+if [[ ! -z "$GITHUB_TOKEN" ]]; then
+  echo Authenticating with GitHub authorisation token
+  AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
+fi
+
+API_URL="https://api.github.com/repos/racerr-io/racerr"
+RELEASE_ID=$(curl -H "$AUTH_HEADER" $API_URL/releases | jq -r "map(select(.prerelease == true))[0].id")
+if [[ -z "$RELEASE_ID" ]]; then
+  echo "Failed to retrieve Racerr prelease."
+  exit 1
+fi
+
+curl \
+  -X DELETE \
+  -H "$AUTH_HEADER" \
+  "$API_URL/releases/$RELEASE_ID"
