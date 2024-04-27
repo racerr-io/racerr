9d2aa7d10f01a2aead281508d654b167bd0ec5f4
Ryan Fallah <ryan_fallah@hotmail.com>
2024-04-27 12:16:15 +1000

    Setup webgl client

--- aws-cdk/lib/client-stack.ts
+++ aws-cdk/lib/client-stack.ts
@@ -4,26 +4,29 @@ import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { assert } from "console";
 
 export const createClientStack = (app: App) => {
-  const lootStack = new Stack(app, "LootStack", {
+  const clientStack = new Stack(app, "racerr-client-stack", {
     env: {
       account: "654654225422",
       region: "ap-southeast-2",
     },
-    crossRegionReferences: true,
   });
 
-  const { cloudFrontWebDistribution, s3Bucket } = new CloudFrontToS3(lootStack, "loot-cloudfront-s3", {
+  const { cloudFrontWebDistribution, s3Bucket } = new CloudFrontToS3(clientStack, "racerr-client", {
     bucketProps: {
       removalPolicy: RemovalPolicy.DESTROY,
+    },
+    logS3AccessLogs: false,
+    cloudFrontDistributionProps: {
+      enableLogging: false
     },
     insertHttpSecurityHeaders: false
   });
 
   assert(s3Bucket);
 
-  // new BucketDeployment(lootStack, 'LootStackS3BucketDeployment', {
-  //   sources: [Source.asset('../build')],
-  //   destinationBucket: s3Bucket!,
-  //   distribution: cloudFrontWebDistribution
-  // })
+  new BucketDeployment(clientStack, 'racerr-client-deployment', {
+    sources: [Source.asset('../build/WebGL/WebGL')],
+    destinationBucket: s3Bucket!,
+    distribution: cloudFrontWebDistribution
+  })
 };
